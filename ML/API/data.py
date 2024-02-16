08d0b900c4904c0fa454c23f64dacdcb2102073b
Pono <ponohuk@gmail.com>
2024-02-16 01:44:41 -0300

    api datasets

--- ML/API/data.py
+++ ML/API/data.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 # Cargar DataFrames desde archivos ubicados en la carpeta datasets_API.
-cities_FL = pd.read_parquet('datasets_API/cities_FL.parquet')
+cities_FL = pd.read_parquet('datasets_API/cities_FL.parquet', engine='pyarrow')
 std_dfs = pd.read_pickle('datasets_API/std_dfs.pkl')
